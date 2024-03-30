39ffa3359e1f2e6bb744a4e95f16e8b34e9a86cd
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2024-03-30 17:18:23 +0530

    add product route and controller

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express();
 
 const userRoute = require('./routes/user');
 const imageRoute = require('./routes/images');
+const productRoute = require('./routes/product');
 
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 
 app.use("/user", userRoute);
 app.use("/images", imageRoute);
+app.use("/products", productRoute);
 
 app.get('/', (req, res) => {
     res.send('Server is working');
