b604e8cbebfc6e9d8b929f21f9d5e5f8d22cce60
Dilhara Sannasgala <kavindu_2020691@fot.ruh.ac.lk>
2024-03-30 23:55:56 +0530

    Updated vehicle controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const adminRoute = require('./routes/admin');
 const employeeRoute = require('./routes/employee');
 const imageRoute = require('./routes/images');
+const vehicleRoute = require('./routes/vehicle');
 
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
@@ -12,6 +13,7 @@ app.use('/uploads', express.static('uploads'));
 app.use("/admin", adminRoute);
 app.use("/employee", employeeRoute);
 app.use("/images", imageRoute);
+app.use("/vehicle", vehicleRoute);
 
 app.get('/', (req, res) => {
     res.send('Server is working');
