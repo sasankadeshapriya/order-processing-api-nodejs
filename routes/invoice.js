b35b66e01023314387c996dc786fc39568f6a804
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2024-04-15 20:18:33 +0530

    add delete and show invoice

--- routes/invoice.js
+++ routes/invoice.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.post('/', invoiceController.createInvoice);
 router.put('/:invoiceId', invoiceController.updateInvoice);
+router.delete('/:invoiceId', invoiceController.deleteInvoice);
+router.get('/:invoiceId', invoiceController.getInvoiceById);
+router.get('/', invoiceController.getAllInvoices);
 
 module.exports = router;
