c38ad69f7c8ee417ac727e7db477b1c26a0e6105
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2024-04-18 13:05:39 +0530

    add delete and get functions with filters

--- routes/vehicleInventory.js
+++ routes/vehicleInventory.js
@@ -5,5 +5,11 @@ const router = express.Router();
 
 router.post('/', vehicleInventoryController.insertVehicleInventory);
 router.put('/:vehicleInventoryId', vehicleInventoryController.updateVehicleInventory);
+router.delete('/:vehicleInventoryId', vehicleInventoryController.deleteVehicleInventory);
+router.get('/:vehicleInventoryId', vehicleInventoryController.getVehicleInventoryById);
+router.get('/', vehicleInventoryController.getAllVehicleInventories);
+
+
+// /vehicle-inventory?in_stock=true --> to get above qty 0
 
 module.exports = router;
