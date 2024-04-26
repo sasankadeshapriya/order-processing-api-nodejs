6d08ea86a911e7644ff6a92b5c2db42f31c0c818
Dilhara Sannasgala <kavindu_2020691@fot.ruh.ac.lk>
2024-04-26 17:24:27 +0530

    Add getEmployeeLocation controller func...

--- routes/employee.js
+++ routes/employee.js
@@ -14,6 +14,7 @@ router.get('/all', employeeController.getAllEmployees);
 router.get('/:employeeId/details', employeeController.getEmployeeDetails);
 router.post('/create', employeeController.createEmployee);
 router.put('/:employeeId/update/location', employeeController.updateEmployeeLocation);
+router.get('/:employeeId/location', employeeController.getEmployeeLocation);
 
 //test
 const testController = require('../controllers/test.controller');
