32a12210e0096132f693891cd711a312a6f3eb8b
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2024-07-08 23:11:16 +0530

    add trash restore

--- controllers/trash.controller.js
+++ controllers/trash.controller.js
@@ -33,6 +33,46 @@ async function getDeletedRecords(req, res) {
     }
 }
 
+async function restoreDeletedRecord(req, res) {
+    const modelName = req.params.model;
+    const id = req.params.id;
+
+    // Validate if the model name provided is valid and exists
+    if (!sequelize.models[modelName]) {
+        return res.status(404).json({
+            message: `Model '${modelName}' not found`
+        });
+    }
+
+    try {
+        // Fetch the deleted record by id
+        const record = await sequelize.models[modelName].findOne({
+            where: { id: id },
+            paranoid: false  // Query also the records marked as deleted
+        });
+
+        if (!record || !record.deletedAt) {
+            return res.status(404).json({
+                message: `Deleted record with id '${id}' not found in model '${modelName}'`
+            });
+        }
+
+        // Restore the deleted record
+        await record.restore();
+
+        return res.status(200).json({
+            message: `Record with id '${id}' restored successfully in model '${modelName}'`
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: 'Something went wrong!',
+            error: error.message
+        });
+    }
+}
+
 module.exports = {
-    getDeletedRecords
+    getDeletedRecords:getDeletedRecords,
+    restoreDeletedRecord:restoreDeletedRecord
 };
--- routes/trash.js
+++ routes/trash.js
@@ -4,6 +4,7 @@ const trashController = require('../controllers/trash.controller');
 const router = express.Router();
 
 router.get('/deletedRecords/:model?', trashController.getDeletedRecords);
-
+// Route for restoring deleted records
+router.put('/restore/:model/:id', trashController.restoreDeletedRecord);
 
 module.exports = router;
